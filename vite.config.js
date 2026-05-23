bc44ceedb0a4ce0149fbf1078fcbed30ce5bd59a
Timothy Zucker <155255765+zuck30@users.noreply.github.com>
2026-05-23 19:48:54 +0300

    Chore: Fixed Cloud Deployment path error to RYOIKI-TENKAI

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/RYOIKI-TENKAI/', 
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/RYOIKI-TENKAI/', 
+export default defineConfig(({ command }) => {
+  const base = command === 'build' ? './' : '/'
+  
+  return {
+    plugins: [react()],
+    base: base,
+  }
 })
